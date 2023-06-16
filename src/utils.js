b52821fd7b683ae0a4ffff108ac62308afbfc093
Mestiz4 <info.mestiz4@gmail.com>
2023-06-15 23:11:20 -0500

    casi term,ino la entrega, falta probar

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+
+let __dirname = dirname(__filename);
+__dirname = __dirname.split("\\helpers").join("");
+export { __dirname };
